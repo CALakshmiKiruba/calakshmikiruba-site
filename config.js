c9f5ecb51a056f865d0d6cd5d2b8f7561def40cf
Lakshmi Kiruba Ananthanarayanan <lakshmikiruba.ka@gmail.com>
2026-09-22 22:25:48 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // Paste your Apps Script web app link between the quotes below.
 // It must start with https://script.google.com/ and end with /exec
-window.SITE_API = "https://script.google.com/macros/s/AKfycbzXSbyqyFP1_IprpPRn_FC7KX_hATc92dM-6NW9DuIuJ600X5U4nyUfVHs0ozdcBbeV/exec";
+window.SITE_API = "https://script.google.com/macros/s/AKfycbzHITakqpAZSS9J_aAQcIle-ECCyPWgDDWklm13c6oa__yllEs3nyRgT2aR9aGP7xal/exec";
