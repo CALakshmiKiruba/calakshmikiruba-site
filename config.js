301040435e581021ccde42a1455d5723bfa42e52
Lakshmi Kiruba Ananthanarayanan <lakshmikiruba.ka@gmail.com>
2026-09-22 20:03:08 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,3 @@
+// Paste your Apps Script web app link between the quotes below.
+// It must start with https://script.google.com/ and end with /exec
+window.SITE_API = "https://script.google.com/macros/s/AKfycbwwSFb09EHD6JQ0O6uHbuSIU41Iw40pIEWE1IAXOGj0rrx-HD2dq6FWMVU0tsQbhT2g/exec";
